2a57384d9f8c90208defc3b6c91874ae71798da7
yata <dragogonone@gmail.com>
2016-12-05 21:54:02 +0900

    rename dev

--- sh/make_test_dev.sh
+++ sh/make_test_dev.sh
@@ -2,14 +2,14 @@
 
 FILENAME=$1
 TEST_LINE=$2
-BATCH_LINE=$3
+DEV_LINE=$3
 
-TB_LINE=`expr ${TEST_LINE} + ${BATCH_LINE}`
+TB_LINE=`expr ${TEST_LINE} + ${DEV_LINE}`
 head -n ${TEST_LINE} ${FILENAME} > ${FILENAME}.test
 echo test ${TEST_LINE}
 
-head -n ${TB_LINE} ${FILENAME} | tail -n ${BATCH_LINE} > ${FILENAME}.batch
-echo batch ${BATCH_LINE}
+head -n ${TB_LINE} ${FILENAME} | tail -n ${DEV_LINE} > ${FILENAME}.dev
+echo batch ${DEV_LINE}
 
 ALL_LINE=$(wc -l ${FILENAME} | awk '{print $1}')
 TRAIN_LINE=`expr ${ALL_LINE} - ${TB_LINE}`
