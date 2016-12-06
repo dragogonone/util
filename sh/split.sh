593faf1662bcef567bcfd587b290d7f04fba4e0f
yata <dragogonone@gmail.com>
2016-12-06 20:20:53 +0900

    add tweet_brush

---
+++ sh/split.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+echo "Usage: this FILENAME LINE"
+exit 1
+fi
+
+FILENAME=$1
+LINE_1=$2
+
+head -n ${LINE_1} ${FILENAME} > ${FILENAME}.1
+
+LINE_FILE=$(wc -l ${FILENAME} | awk '{print $1}')
+echo ${LINE_FILE}
+LINE_2=`expr ${LINE_FILE} - ${LINE_1}`
+tail -n ${LINE_2} ${FILENAME} > ${FILENAME}.2
+||<
