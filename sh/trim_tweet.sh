f8248614fd6e3872867b30b85527ea48eeececf3
yata <dragogonone@gmail.com>
2016-12-08 15:16:44 +0900

    fix space trim

--- sh/trim_tweet.sh
+++ sh/trim_tweet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-sed -e 's/\(RT \)*@.\+[ :]//g' -e 's/\/*[http|t].*//g' -e 's/\\n//g' -e '/^$/d' $1
+sed -e 's/\(RT \)*@.\+[ :]//g' -e 's/\/*[http|t].*//g' -e 's/\\n//g' -e '/^ *$/d' $1
